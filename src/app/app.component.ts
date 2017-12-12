4af5d7a2bb38d7142a291693a035d1641e48611c
Gabriel Rivera Safadi <gabriel.rivera.safadi@gmail.com>
2017-12-12 15:38:14 -0400

    Add line break after each sub indicator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -655,10 +655,10 @@ export class AppComponent {
             } else if (value === 'No') {
               this.footerText = this.footerText + i.label + ': <br /><b>' + i.noText + '</b><br>';
             } else if (i.id == '1a' || i.id == '4' || i.id == '5a' || i.id == '5b' || i.id == '6' || i.id == '9b' || i.id == '10') {
-              this.footerText = this.footerText + i.label + ': ' + (i.prefix + ' <h2>' + value + ' </h2>' + i.suffix) + '<br>';
+              this.footerText = this.footerText + i.label + ': ' + (i.prefix + ' <h2>' + value + ' </h2>' + i.suffix) + '<br><br>';
             } else if (i.id == '2' || i.id == '3') {
               if (value == 'No data available') {
-                this.footerText = this.footerText + i.label + ': ' + (i.prefix + ' ' + value + ' ' + i.suffix) + '<br>';
+                this.footerText = this.footerText + i.label + ': ' + (i.prefix + ' ' + value + ' ' + i.suffix) + '<br><br>';
               } else {
                 this.footerText = this.footerText + i.label + ': ' + (i.prefix + ' <div class="row"><div class="col-md-1"><h2>' + value + ' </h2></div><div class="col-md-11">' + i.suffix) + '</div></div><br>';
               }
@@ -676,12 +676,12 @@ export class AppComponent {
             } else if (subvalue === 'No') {
               this.footerText = this.footerText + '<b>' + j.noText + '</b><br><br>';
             } else if (i.id == '1a' || i.id == '4' || i.id == '5a' || i.id == '5b' || i.id == '6' || i.id == '9b' || i.id == '10') {
-              this.footerText = this.footerText + j.prefix + ' <h2>' + subvalue + ' </h2>' + j.suffix + '<br>';
+              this.footerText = this.footerText + j.prefix + ' <h2>' + subvalue + ' </h2>' + j.suffix + '<br><br>';
             } else if (i.id == '2' || i.id == '3') {
               if (subvalue == 'No data available') {
-                this.footerText = this.footerText + j.prefix + ' <br />' + subvalue + ' ' + j.suffix + '<br>';
+                this.footerText = this.footerText + j.prefix + ' <br />' + subvalue + ' ' + j.suffix + '<br><br>';
               } else {
-                this.footerText = this.footerText + j.prefix + ' <div class="row"><div class="col-md-1"><h2>' + subvalue + ' </h2></div><div class="col-md-11">' + j.suffix + '</div></div><br>';
+                this.footerText = this.footerText + j.prefix + ' <div class="row"><div class="col-md-1"><h2>' + subvalue + ' </h2></div><div class="col-md-11">' + j.suffix + '</div></div><br><br>';
               }
             } else {
               this.footerText = this.footerText + (j.prefix + ' <p>' + subvalue + '</p> ' + j.suffix) + '<br><br>';
