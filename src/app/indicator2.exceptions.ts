11e5fbdb0cba13573df55e25144aeaa4dfaa092c
Lua <xdaddisxd@gmail.com>
2018-02-22 17:25:26 -0400

    exceptions file

---
+++ src/app/indicator2.exceptions.ts
@@ -0,0 +1,90 @@
+export const indicator2Exceptions = [
+  {
+    "id": "2.1",
+    "title": "Module 1 • Availability of spaces for multi-stakeholder dialogue on national development policies",
+    "content": [
+      {
+        "id": "1",
+        "text": "Are civil society organisations (CSOs) consulted by the government in the design, implementation and monitoring of national development policies?"
+      },
+      {
+        "id": "2",
+        "text": "Do CSOs have the right to access government information?"
+      },
+      {
+        "id": "3",
+        "text": "Are there resources and/or training opportunities for addressing capacity building of all stakeholders (including government, CSOs and co-operation providers) to engage meaningfully in multi-stakeholder dialogue?"
+      }
+    ]
+  },
+  {
+    "id": "2.2",
+    "title": "Module 2 • CSO development effectiveness: Accountability and transparency",
+    "content": [
+      {
+        "id": "4",
+        "text": "In practice, are there CSO-managed processes in place to address transparency and multiple accountabilities in CSO operations?"
+      },
+      {
+        "id": "5",
+        "text": "Do CSO-initiated co-ordination processes exist to facilitate consolidated and inclusive CSO representation in policy dialogue (e.g. umbrella organisation, CSO network, consultation practices)?"
+      },
+      {
+        "id": "6",
+        "text": "Do mechanisms exist to facilitate co-ordination on programming among CSOs (collaboration to optimise impact and avoid duplication) and with other development actors?"
+      },
+      {
+        "id": "7",
+        "text": "Are there other signifcant initiatives related to CSO development effectiveness principles (Istanbul Principles and the International Framework for CSO Development Effectiveness) being implemented at the country level?"
+      },
+      {
+        "id": "8",
+        "text": "Do CSOs report annually to government on the basic fnances, sectors of support and main geographic areas of involvement in development?"
+      }
+    ]
+  },
+  {
+    "id": "2.3",
+    "title": "Module 3 • Offcial development co-operation with CSOs",
+    "content": [
+      {
+        "id": "9",
+        "text": "Do providers of development co-operation consult with CSOs on their development policy/programming in a systematic way?"
+      },
+      {
+        "id": "10",
+        "text": "Are providers promoting a CSO enabling environment in their co-operation with civil society?"
+      },
+      {
+        "id": "11",
+        "text": "Is the promotion of a CSO enabling environment an agenda item in providers’ policy dialogue with partner governments?"
+      },
+      {
+        "id": "12",
+        "text": "Do providers share information on their CSO support with the government?"
+      }
+    ]
+  },
+  {
+    "id": "2.4",
+    "title": "Module 4 • Legal and regulatory environment",
+    "content": [
+      {
+        "id": "14",
+        "text": "Is there a recognition of and respect for CSO freedom (association, assembly and expression) in the Constitution and more broadly in policy, law and regulation?"
+      },
+      {
+        "id": "15",
+        "text": "Is the legal and regulatory environment enabling for CSO formation, registration and operation?"
+      },
+      {
+        "id": "16",
+        "text": "Does the legal and regulatory environment facilitate access to resources for CSOs?"
+      },
+      {
+        "id": "17",
+        "text": "Does the legal and regulatory environment marginalise certain groups? "
+      }
+    ]
+  }
+];
