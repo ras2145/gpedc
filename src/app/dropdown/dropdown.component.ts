08f949e2333e0e599cae49a159b3bd6b07ba4bdd
Jorge Monroy Barriga <egrojmonroy@gmail.com>
2018-02-26 12:37:26 -0400

    New dropdown

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../services/loader.service';
+import { ModelService } from '../services/model.service';
 
 @Component({
   selector: 'app-dropdown',
@@ -6,10 +8,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent implements OnInit {
+  categoriesNotNull: any;
+  selectedCountry: any = false;
+  indicatorsSelectedCountry: any;
+  partners: any;
+  categorizedPartners: any;
+  mapTitle: any;
+  geoJson: any;
+  indicatorSelectedFooter: any;
+  footerTab = '';
+  footerText = '';
+  titles: any;
+  years: any;
+  indicator = true;
+  subDropdown: any;
+  subIndicator = false;
+  model = {
+    year: '2016',
+    category: {
+      label: 'Select indicator',
+      title: '',
+      column: '',
+      id: '',
+      legendText: ''
+    },
+    subcategory: null,
+    region: null,
+    incomeGroup: null,
+    countryContext: null
+  };
 
-  constructor() { }
+
+  constructor(
+    private loaderService: LoaderService,
+    private modelService: ModelService
+  ) { }
 
   ngOnInit() {
+    this.titles = this.modelService.getTitles();
   }
-
+  getYears() {
+    this.years = this.modelService.getYears();
+    return this.years;
+  }
+  getIndicators(year) {
+    const categories = this.modelService.getIndicators(year);
+    return categories;
+  }
+  selectCategory(category) {
+    this.model.category = category;
+    this.model.subcategory = null;
+    this.indicator = false;
+    this.subIndicator = true;
+  }
+  unselectCategory() {
+    this.subIndicator = false;
+    this.indicator = true;
+    this.subDropdown = false;
+  }
+  noIsInvalidSelection(category) {
+    const validSelection = (category.id === '7' || category.id === '8' || category.id === '1a' || category.id === '2' || category.id === '3' || category.id === '4');
+    if (!validSelection) {
+      this.subDropdown = false;
+    } else {
+      this.subDropdown = category;
+    }
+    return true;
+  }
+  changeyearLabel(year) {
+    this.unselectCategory();
+  }
+  selectSubcategory(category, subcategory) {
+    this.model.category = category;
+    this.model.subcategory = subcategory;
+    this.subIndicator = false;
+    this.indicator = false;
+  //  console.log(this.model);
+  //  console.log(this.indicator);
+  //  console.log(this.subIndicator);
+  }
+  unselectSubCategory() {
+    this.subIndicator = false;
+    this.model.subcategory = false;
+    const category = this.model.category;
+  }
 }
--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
-
+import { titles } from '../titles';
 @Injectable()
 export class ModelService {
-
+  model = {
+    year: null,
+    category: {
+      label: 'Select indicator',
+      title: '',
+      column: '',
+      id: '',
+      legendText: ''
+    },
+    subcategory: null,
+    region: null,
+    incomeGroup: null,
+    countryContext: null
+  };
+  titles: any;
   constructor() { }
-
+  // return all titles
+  getTitles() {
+    return titles;
+  }
+  // return all data from year
+  getIndicators(year) {
+    this.titles = titles;
+      for (const title of titles) {
+        if (year === title.year) {
+          return title.categories;
+        }
+      }
+  }
+  getYears() {
+    const years = [];
+    for (const title of titles) {
+      years.push(title.year);
+    }
+    return years;
+  }
 }
