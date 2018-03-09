b2d63230ad9b69dfd1fb3798dc665abe93b158d2
Jorge Monroy Barriga <egrojmonroy@gmail.com>
2018-03-09 11:21:57 -0400

    Remove old table

--- src/app/server.config.ts
+++ src/app/server.config.ts
@@ -5,7 +5,7 @@ export const SERVER = (function () {
     const API_KEY = `&api_key=${CARTO_TOKEN}`;
     const FORMAT = '&format=GeoJSON';
     const BASE_URL = `https://${USERNAME}.carto.com/api/v2`;
-    const COUNTRY_TABLE = 'undp_countries_copy';
+   // const COUNTRY_TABLE = 'undp_countries_copy';
     const PARTNER_TABLE = 'gpedc_screen_3';
     const COLUMS_OF_COUNTRIES = '_2016_1_2,iso3_code,status,country,_2016,_2014,region,inc_group,ldc,sids,background,profile,_2014_5a,_2014_5b,_2014_6,_2014_7,_2014_7_1,_2014_7_2,_2014_7_3,_2014_7_4,_2014_7_5,_2014_8,_2014_8_1,_2014_8_2,_2014_8_3,_2014_8_4,_2014_9a,_2014_9b,_2014_10,_2016_1_1,cartodb_id,_2016_1_3,_2016_1_4,_2016_2_1,_2016_2_2,_2016_2_3,_2016_2_4,_2016_3_1,_2016_3_2,_2016_3_3,_2016_3_4,_2016_4_1,_2016_4_2,_2016_4_3,_2016_5a,_2016_5b,_2016_6,_2016_7,_2016_7_1,_2016_7_2,_2016_7_3,_2016_7_4,_2016_7_5,_2016_8,_2016_8_1,_2016_8_2,_2016_8_3,_2016_8_4,_2016_9a,_2016_9b,_2016_10';
     // const COLUMS_OF_COUNTRIES = '_2016_1_2,iso3_code,status,country,_2016,_2014,region,inc_group,ldc,sids,background,profile,_2014_5a,_2014_5b,_2014_6,_2014_7,_2014_7_1,_2014_7_2,_2014_7_3,_2014_7_4,_2014_7_5,_2014_8,_2014_8_1,_2014_8_2,_2014_8_3,_2014_8_4,_2014_9a,_2014_9b,_2014_10,_2016_1_1,cartodb_id,_2016_1_3,_2016_1_4,_2016_2_1,_2016_2_2,_2016_2_3,_2016_2_4,_2016_3_1,_2016_3_2,_2016_3_3,_2016_3_4,_2016_4_1,_2016_4_2,_2016_4_3,_2016_5a,_2016_5b,_2016_6,_2016_7,_2016_7_1,_2016_7_2,_2016_7_3,_2016_7_4,_2016_7_5,_2016_8,_2016_8_1,_2016_8_2,_2016_8_3,_2016_8_4,_2016_9a,_2016_9b,_2016_10';
@@ -16,7 +16,7 @@ export const SERVER = (function () {
         API_KEY: API_KEY,
         BASE_URL: BASE_URL,
         CARTO_TOKEN: CARTO_TOKEN,
-        COUNTRY_TABLE: COUNTRY_TABLE,
+        //COUNTRY_TABLE: COUNTRY_TABLE,
         PARTNER_TABLE: PARTNER_TABLE,
         GPEDC_SCREENS_1_2: GPEDC_SCREENS_1_2,
         GPEDC_SCREENS_1_2_WITHOUT_GEOMETRY: GPEDC_SCREENS_1_2_WITHOUT_GEOMETRY,
