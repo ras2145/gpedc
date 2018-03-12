087546ed3d5e9b758f42f8297f359510773defe7
Lua <xdaddisxd@gmail.com>
2018-03-12 17:25:31 -0400

    add country historical logic

--- src/app/country-historical/country-historical.component.ts
+++ src/app/country-historical/country-historical.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { analysisData } from '../../app/analysisData';
 import { Subject } from 'rxjs/Subject';
+import { CountryAnalysisService } from '../services/country-analysis.service';
+import * as d3 from 'd3';
 @Component({
   selector: 'app-country-historical',
   templateUrl: './country-historical.component.html',
@@ -9,21 +11,33 @@ import { Subject } from 'rxjs/Subject';
 export class CountryHistoricalComponent implements OnInit {
   titleSubject: Subject <any> = new Subject();
   filters;
+  charttext;
+  countries: any;
+  selectedCountry = 'Select Country';
+  chartData: any;
   model = {   };
   indicator = {
     dropdowncountry: 'Select an indicator',
     subdropdown: [],
-    titlecountry: ''
+    titlecountry: '',
+    indicator: '',
+    charttext: ''
   };
   subIndicator = {
     subdropdown: 'Select Sub-Indicator',
-    titlecountry: ''
+    titlecountry: '',
+    indicator: '',
+    charttext: ''
   };  
   subDropdown = false;
   title = '';
-  constructor() { 
+  constructor(private countryAnalysisService: CountryAnalysisService) { 
   }
   ngOnInit() {
+    this.countryAnalysisService.getCountries().subscribe(res => {
+      this.countries = res;
+      console.log(this.countries);
+    });
     this.filters = analysisData;
     this.filters.forEach(filter => {
       if (filter.year === 2016) {
@@ -31,18 +45,55 @@ export class CountryHistoricalComponent implements OnInit {
       }
     });
   }
+  resetCountry() {
+    this.selectedCountry = 'Select Country';
+    this.sendTitle();
+  }
+  changeCountry(country) {
+    this.selectedCountry = country;
+    this.sendTitle();
+    console.log(this.selectedCountry);
+  }
+  run() {
+    console.log(this.indicator);
+    console.log(this.subIndicator);
+    console.log(this.selectedCountry);
+    console.log(this.model['year']);
+    if (this.selectedCountry === 'Select Country') {
+      alert('Please select a country');
+    }
+    if (this.indicator.dropdowncountry === 'Select an indicator') {
+      alert('Please select an Indicator');
+      return;
+    }
+    if (this.subDropdown) {
+      if (this.subIndicator.subdropdown !== 'Select Sub-Indicator') {
+        this.countryAnalysisService.getCharData(this.selectedCountry, this.subIndicator.indicator, this.model['year']).subscribe(res => {
+          this.chartData = res;
+          this.draw();
+        });
+      } else {
+        alert('Please select a Sub-Indicator');
+        return;
+      }
+    } else {
+      this.countryAnalysisService.getCharData(this.selectedCountry, this.indicator.indicator, this.model['year']).subscribe(res => {
+        this.chartData = res;
+        this.draw();
+      });
+    }
+  }
   changeIndicator(indicator) {
     this.indicator = indicator;
     console.log(this.indicator);
-    this.title = (this.indicator.titlecountry ? this.indicator.titlecountry : '');
     this.sendTitle();
     this.subDropdown = (this.indicator.subdropdown.length > 0);
+    this.charttext = this.indicator.charttext;
     this.subIndicator.subdropdown = 'Select Sub-Indicator';
     if (this.subDropdown) {
       this.indicator.subdropdown.forEach(sub => {
         if (sub.autoselect) {
           this.subIndicator = sub;
-          this.title = (this.subIndicator.titlecountry ? this.subIndicator.titlecountry : '');
           this.sendTitle();
         }    
       });
@@ -50,34 +101,63 @@ export class CountryHistoricalComponent implements OnInit {
   }
   changeSubIndicator(subIndicator) {
     this.subIndicator = subIndicator;
-    this.title = (this.subIndicator.titlecountry ? this.subIndicator.titlecountry: '');
+    this.charttext = this.subIndicator.charttext;
     this.sendTitle();
   }
   changeYear(year) {
     this.reset();
   }
   sendTitle() {
+    this.title = (this.indicator.titlecountry ? this.indicator.titlecountry : '');
+    if (this.subDropdown && this.subIndicator.subdropdown !== 'Select Sub-Indicator') {
+      this.title = (this.subIndicator.titlecountry ? this.subIndicator.titlecountry : '');
+      this.charttext = this.subIndicator.charttext;
+    }
     this.title = this.title.replace('[YEAR]', this.model['year']);
+    this.title = this.title.replace('[DEVELOPING COUNTRY]', this.selectedCountry);
+    if (this.selectedCountry === 'Select Country') {
+      this.title = '';
+    }
   }
   reset() {
     this.indicator = {
       dropdowncountry: 'Select an indicator',
       subdropdown: [],
-      titlecountry: ''
+      titlecountry: '',
+      indicator: '',
+      charttext: ''
     };
     this.subIndicator = {
       subdropdown: 'Select Sub-Indicator',
-      titlecountry: ''
+      titlecountry: '',
+      indicator: '',
+      charttext: ''
     };
     this.subDropdown = false;
+    this.title = '';
+    this.selectedCountry = 'Select Country';
+    this.charttext = '';
   }
   resetSub() {
     this.subIndicator = {
       subdropdown: 'Select Sub-Indicator',
-      titlecountry: ''
+      titlecountry: '',
+      indicator: '',
+      charttext: ''
     };
+    this.charttext = '';
+    this.title = (this.indicator.titlecountry ? this.indicator.titlecountry : '');
   }
   show(event) {
     console.log(event);
+  }
+  draw() {
+    const margin = {top: 20, right: 20, bottom: 30, left: 40};
+    const width = 400;
+    const height = 600;
+    let svg = d3.select("#chart").append("svg:svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g");
   }
 }
